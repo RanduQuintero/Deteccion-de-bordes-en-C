a75e0a617248ad68196933a2e53835bb7b1629d7
RanduQuintero <randuquintero@hotmail.com>
2026-01-31 20:29:10 -0700

    Instrucciones

---
+++ Bordes.py
@@ -0,0 +1,7 @@
+
+#Esto es para indicar que el proyecto fue elaborado y ejecutado
+#en un sistema operativo base Linux (Ubuntu) y con manejo de la 
+#memoria 
+
+Linux = "Sistema operativo"
+print(Linux)
